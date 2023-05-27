77db84f50add883973e2b46e35873daab6073f19
Tran Thu Trang <trangtthe161410@fpt.edu.vn>
2023-05-27 18:39:14 +0700

    upadaete

--- HelloWorld/src/Main/Test.java
+++ HelloWorld/src/Main/Test.java
@@ -4,7 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("TEST");
 		System.out.println("New Brach");
-		System.out.println("Thu Trang");
+		System.out.println("Thu Trang 123")
+		
 		
 	}
 }
